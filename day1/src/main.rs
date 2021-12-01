77afb2f1434a3b13ec8e1df0cc1565e158b35be6
Leon von Mulert <leonvonmulert@gmail.com>
2021-12-01 16:06:27 +0100

    Day 1 complete.

---
+++ day1/src/main.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+
+const INPUT: &str = include_str!("input.txt");
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let meters: Vec<i32> = INPUT.lines().map(|l|l.trim().parse().unwrap()).collect();
+    let increases = meters.windows(2).filter(|w|w[0] < w[1]).count();
+
+    println!("There have been {} increases in depth.", increases);
+
+    let measurement_windows: Vec<i32> = meters.windows(3).map(|w| w[0] + w[1] + w[2]).collect();
+    let window_increases = measurement_windows.windows(2).filter(|w|w[0] < w[1]).count();
+    println!("There have been {} windowed increases in depth.", window_increases);
+
+    Ok(())
+}
